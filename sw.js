4f7836eb97ccda53823f2ff8f513588f6634fd4a
Angel0973180707 <a0973180707@gmail.com>
2026-02-10 19:10:20 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,17 @@
 const CACHE_NAME = 'happy-smart-v1';
 
 // 定義需要快取的檔案清單
-const ASSETS_TO_CACHE = [
+const ASSETS_Tconst CACHE_NAME = 'happy-v1';
+const ASSETS = ['index.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
+O_CACHE = [
   'index.html',
   'manifest.json',
   'icon-192.png',
