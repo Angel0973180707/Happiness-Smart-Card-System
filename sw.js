d9f6f64daf97bd2a675c3ba9026ad5994e0f9e22
Angel0973180707 <a0973180707@gmail.com>
2026-02-10 19:05:21 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,41 @@
-const CACHE_NAME = 'happiness-v1';
-const ASSETS = ['index.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
+const CACHE_NAME = 'happy-smart-v1';
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+// 定義需要快取的檔案清單
+const ASSETS_TO_CACHE = [
+  'index.html',
+  'manifest.json',
+  'icon-192.png',
+  'icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // 優先回傳快取，若無則連網抓取
+      return response || fetch(event.request);
+    })
+  );
 });
