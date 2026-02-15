2175b8b4178615185efba08b3fb24f9c9ffef9ac
Angel0973180707 <a0973180707@gmail.com>
2026-02-15 12:59:57 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* 天使幸福智慧名片 SW v337*/
-const CACHE_VERSION = "v337";
+/* 天使幸福智慧名片 SW v338*/
+const CACHE_VERSION = "v338";
 const CACHE_NAME = `angel-card-${CACHE_VERSION}`;
 
 const CORE_ASSETS = [
