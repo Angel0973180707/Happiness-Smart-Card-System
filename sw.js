111d726e7aa6067fd040ed9a3cf5b520015bd4a4
Angel0973180707 <a0973180707@gmail.com>
2026-02-19 12:59:14 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-/* Angel Card SW v384- cache-busted */
-const CACHE = "angel-card-v384";
+/* Angel Card SW v385- cache-busted */
+const CACHE = "angel-card-v385";
 const ASSETS = [
   "./",
   "./index.html",
-  "./style.css?v=384",
-  "./app.js?v=384",
-  "./manifest.json?v=384",
+  "./style.css?v=385",
+  "./app.js?v=385",
+  "./manifest.json?v=385",
   "./icons/icon-192.png",
   "./icons/icon-512.png"
 ];
