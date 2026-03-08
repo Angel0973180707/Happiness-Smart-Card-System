a355abcfc82e5723c7ba5ad20b0b5a73f8b8b0d4
Angel0973180707 <a0973180707@gmail.com>
2026-03-09 06:50:24 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* =========================================
- * HSC Service Worker v703
+ * HSC Service Worker v705
  * COMPLETE OVERWRITE
  *
  * Goal:
@@ -10,7 +10,7 @@
  * - HTML / version.json 走網路優先
  * ========================================= */
 
-const SW_VERSION = "v703";
+const SW_VERSION = "v705";
 const CACHE_NAME = `hsc-cache-${SW_VERSION}`;
 
 const CORE_ASSETS = [
