9693fc435a65c1accef1f017f794bc8c3334efbe
Angel0973180707 <a0973180707@gmail.com>
2026-03-22 14:10:36 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* =========================================
- * HSC Service Worker v524.9
+ * HSC Service Worker v4.7
  * COMPLETE OVERWRITE
  *
  * Goal:
@@ -10,7 +10,7 @@
  * - HTML / version.json 走網路優先
  * ========================================= */
 
-const SW_VERSION = "v524.9";
+const SW_VERSION = "v4.7";
 const CACHE_NAME = `hsc-cache-${SW_VERSION}`;
 
 const CORE_ASSETS = [
