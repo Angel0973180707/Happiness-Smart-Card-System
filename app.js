760c8898b1a36da5f19fd9a5434657c02b3d9049
Angel0973180707 <a0973180707@gmail.com>
2026-05-15 00:09:35 +0800

    Update app.js

--- app.js
+++ app.js
@@ -849,12 +849,7 @@ async function loadFacadeBaseCard(){
 
   // 無快取：顯示骨架屏，再等 GAS
   renderSkeletonUI_();
- let shellPayload;
-try {
-  shellPayload = await fetchJsonRobust_(buildStaticCardUrl_(cardId) + '-shell', { cacheMode: "default" });
-} catch(_e) {
-  shellPayload = await fetchJsonRobust_(buildCardPublicShellUrl_(cardId), { cacheMode: "default" });
-}
+ const shellPayload = await fetchJsonRobust_(buildCardPublicShellUrl_(cardId), { cacheMode: "default" });
   const shell = extractCardRow_(shellPayload);
   if(!shell || typeof shell !== "object" || !Object.keys(shell).length){
     throw new Error("門面樣品卡 shell 資料為空");
